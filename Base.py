6b7444ba95b7494391a3481d539f4a5730d6c4ce
Darkkyelfo <raulpedrouag@gmail.com>
2017-10-17 00:38:16 -0300

    atualização na classe Base. Nada de mais

--- Base.py
+++ Base.py
@@ -8,7 +8,6 @@ class Base(object):
     '''
     classdocs
     '''
-
     
     def __init__(self, classes,atributos):
         self.tiposClasses = []
@@ -17,4 +16,16 @@ class Base(object):
         for e in classes:
             if(e not in self.tiposClasses):
                 self.tiposClasses.append(e) 
+    
+    
+    def getSubBaseClasse(self,indice):
+        subClasse = []
+        subAtributos = []
+        for i,e in enumerate(self.classes):
+            if(e == self.tiposClasses[indice]):
+                subClasse.append(e)
+                subAtributos.append(self.atributos[i])
+        return Base(subClasse,subAtributos)
+                
+        
         
